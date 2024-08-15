569ab0bfbfc859bf9212c2e4c4583de36ee40957
Richard Addo <ramankwah03@gmail.com>
2024-08-15 21:38:51 +0200

    test modified

--- __test__/complete.test.js
+++ __test__/complete.test.js
@@ -1,11 +1,11 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from '../App.jsx';
+import CompletedForm from '../src/components/Completed.js';
 
 describe('App tests', () => {
     it('should contains the heading 1', () => {
-        render(<App />);
-        const heading = screen.getByText(/Hello world! I am using React/i);
+        render(<CompletedForm />);
+        const heading = screen.getByText(/ Thanks /i);
         expect(heading).toBeInTheDocument()
     });
 });
